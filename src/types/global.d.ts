1a06cc75e647988763f805c4693ccf2c83c16ae3
Ray Lee <fsdp0dev@gmail.com>
2024-12-06 15:33:14 +0900

    [FSDP-GLOB-0001] Automation Commit

---
+++ src/types/global.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends Dict<string>, Record<string, string | number> {
+      NODE_ENV: "local" | "development" | "production";
+    }
+  }
+}
+
+export = global;
